e7b235119a3ad4b26b5ca89f3a6dd34f509883b3
Denise Nadal <denisenadal@outlook.com>
2017-03-20 10:44:05 -0600

    Editing

---
+++ calList-edited.js
@@ -0,0 +1,8 @@
+//GLOBAL VARIABLES - these are samples, real values are obscured
+var API_KEY = apikey;
+var SUBJECT_LIST = ["math","statistics","foreign language"];
+var WEEK_OFFSET = null;
+var CAL_COUNT = 0;
+var COLORS = ["#741115","#f1141a","#ba1c21","#df4a2c","#97230b","#de090f"];
+var CAL_LIST = [
+	{"subject": "subjectName","calID" : "Google Calendar ID","sessions":[], "icon" : '<svg><use xlink:href="icon_set.svg#subject"></use></svg>'}];
